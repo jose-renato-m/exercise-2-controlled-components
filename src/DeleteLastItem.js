0b9cef766417964ef5d5887e6b9701793b4d2645
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-22 21:45:46 +0000

    feat: Add variable which pass props to variable

--- src/DeleteLastItem.js
+++ src/DeleteLastItem.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const DeleteLastItem = props => {
+  const handleDeleteLastItem = event => {
+    props.onDeleteLastItem();
+  };
+  
+  
+};
+
 export default DeleteLastItem;
