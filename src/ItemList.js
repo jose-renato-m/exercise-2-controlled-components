6f5f2e612af1e0597a907df51cb47628dfccb072
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-22 20:47:16 +0000

    feat: Add variable to pass props returning jsx

--- src/ItemList.js
+++ src/ItemList.js
@@ -2,4 +2,12 @@ import React from 'react';
 import Item from './Item';
 import PropTypes from 'prop-types';
 
+const ItemList = props => {
+	return (
+    	<div>
+      	  <p className='items'>Items</p>
+      	</div>
+    );
+};
+
 export default ItemList;
