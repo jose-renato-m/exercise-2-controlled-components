779ef5fe4c6e10ba052a8d2cf2de54ad1a2db7e8
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-22 21:16:32 +0000

    feat: Manage item addition

--- src/CreateNewItem.js
+++ src/CreateNewItem.js
@@ -9,6 +9,11 @@ class CreateNewItem extends React.Component {
 	handleChange = event => {
       this.setState({ value: event.target.value });
     };
+
+	addItem = event => {
+      event.preventDefault();
+      this.props.onAddItem(this.state.value);
+    };
 };
 
 export default CreateNewItem;
