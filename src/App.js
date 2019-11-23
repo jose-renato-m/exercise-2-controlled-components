694f382dd2a38815e7c5e110882b9116c876f492
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-11-23 00:11:19 +0000

    feat: Add state if no items found

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ class App extends React.Component {
     this.setState(prevState => ({ items: this.state.items.slice(0, -1)}));
   };
 
+  noItemsFound = () => {
+    return this.state.items.length === 0;
+  };
+
   handleChange = event => {
     this.setState({ value: event.target.value });
   };
